a05668d778246e0a494d9824932451b13dc4f60a
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2024-12-04 21:15:42 +0500

    rasmlar almashdi

--- src/pages/searchingPage/constanta.js
+++ src/pages/searchingPage/constanta.js
@@ -2,96 +2,96 @@ export const genres = [
     {
       "id": 28,
       "name": "Action",
-      "image" : 'src/assets/action.png'
+      "image" : '/genres/action.png'
     },
     {
       "id": 12,
       "name": "Adventure",
-      "image" : 'src/assets/adventure.webp'
+      "image" : '/genres/adventure.webp'
     },
     {
       "id": 16,
       "name": "Animation",
-      "image" : 'src/assets/animated-genre.jpg'
+      "image" : '/genres/animated-genre.jpg'
     },
     {
       "id": 35,
       "name": "Comedy",
-      "image" : 'src/assets/comedy.png'
+      "image" : '/genres/comedy.png'
     },
     {
       "id": 80,
       "name": "Crime",
-      "image" : 'src/assets/criminal.png'
+      "image" : '/genres/criminal.png'
     },
     {
       "id": 99,
       "name": "Documentary",
-      "image" : 'src/assets/documentary.png'
+      "image" : '/genres/documentary.png'
     },
     {
       "id": 18,
       "name": "Drama",
-      "image" : 'src/assets/drama.webp'
+      "image" : '/genres/drama.webp'
     },
     {
       "id": 10751,
       "name": "Family",
-      "image" : 'src/assets/family.png'
+      "image" : '/genres/family.png'
     },
     {
       "id": 14,
       "name": "Fantasy",
-      "image" : 'src/assets/fantasy.webp'
+      "image" : '/genres/fantasy.webp'
     },
     {
       "id": 36,
       "name": "History",
-      "image" : 'src/assets/troya history.jpg'
+      "image" : '/genres/troya history.jpg'
     },
     {
       "id": 27,
       "name": "Horror",
-      "image" : 'src/assets/horror.jpg'
+      "image" : '/genres/horror.jpg'
     },
     {
       "id": 10402,
       "name": "Music",
-      "image" : 'src/assets/melodrama.webp'
+      "image" : '/genres/melodrama.webp'
     },
     {
       "id": 9648,
       "name": "Mystery",
-      "image" : 'src/assets/mystery.jpg'
+      "image" : '/genres/mystery.jpg'
     },
     {
       "id": 10749,
       "name": "Romance",
-      "image" : 'src/assets/romance.avif'
+      "image" : '/genres/romance.avif'
     },
     {
       "id": 878,
       "name": "Science Fiction",
-      "image" : 'src/assets/science-fiction.png'
+      "image" : '/genres/science-fiction.png'
     },
     {
       "id": 10770,
       "name": "TV Movie",
-      "image" : 'src/assets/serial.webp'
+      "image" : '/genres/serial.webp'
     },
     {
       "id": 53,
       "name": "Thriller",
-      "image" : 'src/assets/triller.png'
+      "image" : '/genres/triller.png'
     },
     {
       "id": 10752,
       "name": "War",
-      "image" : 'src/assets/war.jpg'
+      "image" : '/genres/war.jpg'
     },
     {
       "id": 37,
       "name": "Western",
-      "image" : 'src/assets/westerns-cover.jpg'
+      "image" : '/genres/westerns-cover.jpg'
     }
   ]
